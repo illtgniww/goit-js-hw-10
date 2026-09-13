f59309c4a7a4e03fe990f70438cdcb263825af3a
illtgniww <antongolovcuk8@gmail.com>
2026-09-13 12:13:00 +0000

    Deploying to gh-pages from @ illtgniww/goit-js-hw-10@54469ed622f236e65140b6ffd8f03ad268dcac99 🚀

---
+++ 2-snackbar.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */import{i as r}from"./assets/vendor-BZoxUzx5.js";const o=document.querySelector(".form");o.addEventListener("submit",t=>{t.preventDefault();const s=Number(t.target.elements.delay.value),i=t.target.elements.state.value;new Promise((e,m)=>{setTimeout(()=>{i==="fulfilled"?e(s):m(s)},s)}).then(e=>{r.success({title:"Success",message:`✅ Fulfilled promise in ${e}ms`})}).catch(e=>{r.error({title:"Error",message:`❌ Rejected promise in ${e}ms`})})});
+//# sourceMappingURL=2-snackbar.js.map
