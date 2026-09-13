4ccacebc34cdbcb954b2ef2376eb9329531f53fd
Anton <antongolovcuk8@gmail.com>
2026-09-13 15:31:53 +0300

    hw4

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -19,7 +19,7 @@ const options = {
   onClose(selectedDates) {
       console.log(selectedDates[0]);
       userSelectedDate = selectedDates[0];
-      if (userSelectedDate < new Date()) {
+      if (userSelectedDate <= new Date()) {
         iziToast.error({message: 'Please choose a date in the future', position: 'topRight'});
         startBtn.disabled = true;
       }
